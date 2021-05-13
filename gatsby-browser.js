0d8f9aea36dfbeeff3a16b06e975af59f41327ea
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-05-12 20:45:37 -0700

    added netlify

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,9 +4,10 @@ import store from './src/Global/store';
 import { Theme } from "./src/Global/theme";
 import './src/Global/Global.css';
 import { ApolloProvider } from '@apollo/client';
-import { client } from './src/apollo/client';
+import { client } from "./src/Apollo/client";
+import {Providers} from './netlifyIdentityContext';
 
 export const wrapRootElement = ({ element }) => {
   
-  return <ApolloProvider client={client}><Provider store={store}><Theme>{element}</Theme></Provider></ApolloProvider>;
+  return <Providers><ApolloProvider client={client}><Provider store={store}><Theme>{element}</Theme></Provider></ApolloProvider></Providers>;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,61 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { useQuery, useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to Gatsby starter.</p>
-    <p> It includes:</p>
-    <ul>
-      <li>Typescript</li>
-      <li>Material-ui</li>
-      <li>Redux toolkit</li>
-      <li>Dark Theme</li>
-      <li>dotenv</li>
-    </ul>
-    <p>In order to change the title, description and other details go to gatsby-config file and edit the title</p>
-  </Layout>
-)
+export const GET_TODO = gql`
+{
+  bookmarks {
+    desc,
+    url,
+    id,
+    date,
+  }
+}
+`;
 
+const IndexPage = () => {
+  const { loading, error, data } = useQuery(GET_TODO);
+
+  if (loading) {
+    return (
+      <Layout>
+        <SEO title="Todo" />
+        <h1>Loading...</h1>
+      </Layout>
+    );
+  }
+
+  if (error) {
+    return (
+      <Layout>
+        <SEO title="Todo" />
+        <h3>Error, Please try again later</h3>
+      </Layout>
+    );
+  }
+
+  if (data) {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <h1>Hi people</h1>
+        <p>Welcome to Gatsby starter.</p>
+        <p> It includes:</p>
+        <ul>
+          <li>Typescript</li>
+          <li>Material-ui</li>
+          <li>Redux toolkit</li>
+          <li>Dark Theme</li>
+          <li>dotenv</li>
+          <li>apollo</li>
+          <li>graphql</li>
+          <li>faunadb</li>
+        </ul>
+        <p>In order to change the title, description and other details go to gatsby-config file and edit the title</p>
+      </Layout>
+    );
+  }
+}
 export default IndexPage
